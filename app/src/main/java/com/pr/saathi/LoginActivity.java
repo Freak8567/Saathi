02a0e36c1fbcac4b35765c1b0f2585ca5dc84832
RISHABH <rish@RISHABHs-MacBook-Pro.local>
2016-05-16 19:05:49 +0530

    added login and signup activity class files along with there layout files

---
+++ app/src/main/java/com/pr/saathi/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.pr.saathi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rish on 16/05/16.
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
---
+++ app/src/main/java/com/pr/saathi/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.pr.saathi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rish on 16/05/16.
+ */
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+
+    }
+}
